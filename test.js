8e687fe5cde5223a905c984051c460e7133486d1
Shawn Nagle <shawnnaglecanada@gmail.com>
2017-07-12 17:59:20 -0400

    Update test.js

--- test.js
+++ test.js
@@ -17,26 +17,27 @@ function getSleepHours(day) {
 }
 
 function getAcualSleepHours() {
-var getAcualSleepHours = (getSleepHours()('monday') + getSleepHours()('tuesday') + getSleepHours()('wensday') + getSleepHours()('thursday') + getSleepHours()('friday') + getSleepHours()('saterday') + getSleepHours()('sunday'));
-return  getAcualSleepHours();
+  return getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wensday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saterday') + getSleepHours('sunday');
+// Totals all the hours  of sleep from getSleepHours that the user got
 }
 
 function getIdealSleepHours() {
   var idealHours = 7;
   return idealHours * 7;
+  // Returns the total number of ideal sleep hours  per week that the user picked
 }
 
 
 function calculateSleepDebt() {
 var acualSleepHours = getAcualSleepHours();
 var idealSleepHours = getIdealSleepHours();
+ 
   if (acualSleepHours === idealSleepHours) {
-   console.log('You got exacly the amount of sleep you wanted');
- }
-  if (acualSleepHours < idealSleepHours); {
-   console.log('you got ' + (idealSleepHours - acualSleepHours) + 'hours less then your ideal sleep');
- }
-  if (acualSleepHours > idealSleepHours);
-   console.log('You got ' + (acualSleepHours - idealSleepHours) + 'more sleep then you planned');
- }
+   console.log('You got exacly the amount of sleep you wanted'); 
+  } if (acualSleepHours < idealSleepHours); {
+        console.log('you got ' + (idealSleepHours - acualSleepHours) + ' hours less then your ideal sleep'); 	
+     else if (acualSleepHours > idealSleepHours); {
+        console.log('You got ' + (acualSleepHours - idealSleepHours) + ' hours more sleep then you planned');
+     }}}
+
 calculateSleepDebt();
